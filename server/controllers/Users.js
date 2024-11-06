9d2aad569f26e1df48ce95c51cefef720d39797f
unknown <bhavyashukla23@gmail.com>
2024-11-06 22:51:22 +0530

    added user controller

---
+++ server/controllers/Users.js
@@ -0,0 +1,31 @@
+import User from '../models/userModel.js';
+
+
+module.exports ={
+    getById : async(req , res)=>{
+       try{
+          const { id } = req.params;
+          let result = await User.findById(id);
+          result=result.toObject();
+          delete result.password;
+          res.status(200).json(result);
+       }catch(error){
+         console.error("Error is:" + error);
+         res.status(500).json({message: 'Error while getting details, try again later'});
+       }
+   },
+
+   updateById : async(req , res)=>{
+      try{
+           const { id } = req.params;
+           let result = await User.findByIdAndUpdate(id , req.body, { new: true });
+           result.toObject();
+           delete result.password;
+           res.status(200).json(updated);
+
+      }catch(error){
+        console.log("Error is:"+ error );
+        res.status(500).json({message:'Error while updating details, try again later'});
+      }
+   },
+};
